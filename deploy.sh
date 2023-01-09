3976786725516adb36479c6a7d6d00b54fee3044
lyc2014 <1006414710@qq.com>
2023-01-10 07:52:28 +0800

    feat 执行栈和执行上下文

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
+git config user.name "lyc2014"
+git config user.email "1006414710@qq.com"
 git commit -m 'deploy'
 
 git push -f git@github.com:lyc2014/blog.git master:gh-pages
